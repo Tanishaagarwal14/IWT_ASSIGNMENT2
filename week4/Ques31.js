059d16502a524e574a3b24e8fb9bceb144f699bb
Tanishaagarwal14 <86571632+Tanishaagarwal14@users.noreply.github.com>
2023-03-30 22:01:59 +0300

    Add files via upload

---
+++ week4/Ques31.js
@@ -0,0 +1,6 @@
+function change()
+{
+	document.getElementbyId('display').innerHTML=document.forms[0].elements['range'].value;
+	document.getElementbyId('style.left').innerHTML=document.forms[1].elements['range'].value;
+	}
+	form.elments[100].value=result;
